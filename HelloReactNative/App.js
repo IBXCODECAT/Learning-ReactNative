fa7657b61ee2ee4e7fc34674c4fa21ad1c49edf2
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-02-02 13:54:35 -0600

    HelloReactNative

---
+++ HelloReactNative/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
