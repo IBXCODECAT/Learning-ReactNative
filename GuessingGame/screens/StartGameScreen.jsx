f5537f4f917cef5a03d2723f28a2eb9131d5fe9c
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-02-24 17:10:05 -0600

    Text (Number) Input

--- GuessingGame/screens/StartGameScreen.jsx
+++ GuessingGame/screens/StartGameScreen.jsx
@@ -4,7 +4,12 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() { 
     return (
         <View style={styles.inputContainer}>
-            <TextInput/>
+            <TextInput 
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCorrect={false}
+                />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -29,5 +34,17 @@ const styles = StyleSheet.create({
         shadowOffset: {width: 0, height: 2},
         shadowRadius: 6,
         shadowOpacity: 0.25,
+    },
+
+    numberInput: {
+        height: 60,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
     }
 });
