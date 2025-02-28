fea0017edf3a660133c989ca398e5f23758ffc4e
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-02-28 12:33:21 -0600

    Bounds Checking + Buttons

--- GuessingGame/screens/GameScreen.jsx
+++ GuessingGame/screens/GameScreen.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import { useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 // Exclude the user's number from the random number generation
 // This is used to prevent the phone from winning the game the first round
@@ -16,16 +17,44 @@ function generateRandomNumber(min, max, exclude) {
     }
 }
 
+let minBounds = 0;
+let maxBounds = 100;
+
 function GameScreen({userChoice}) {
-    const initialGuess = generateRandomNumber(1, 100, userChoice);
+    const initialGuess = generateRandomNumber(minBounds, maxBounds, userChoice);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    function nextGuessHandler(isGreater /*boolean*/) {
+        
+        if (
+            (isGreater === false && currentGuess < userChoice) ||
+            (isGreater === true && currentGuess > userChoice)
+        ) {
+            Alert.alert("Don't lie!", "You know this is wrong...", [{text: 'Sorry!', style: 'cancel'}]);
+            return;
+        }
+        
+        if(isGreater === true) {
+            minBounds = currentGuess + 1; // Min boundry is inclusive, so we add 1 to the current guess
+        } else {
+            maxBounds = currentGuess; // Max boundry is exclusive, so we set it to the current guess
+        }
+
+        // Generate a new random number between the min and max bounds
+        const newGuess = generateRandomNumber(minBounds, maxBounds, currentGuess);
+        setCurrentGuess(newGuess);
+    }
 
     return (
         <View style={styles.screen}>
             <Title>Oponent's Guess</Title>
             <NumberContainer>{currentGuess}</NumberContainer>
             <View>
-                
+                <Text>Higher or Lower?</Text>
+                <View>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, false)}>-</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, true)}>+</PrimaryButton>
+                </View>
             </View>
         </View>
     )
