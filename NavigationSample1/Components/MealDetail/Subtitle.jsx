ae9613ae1bd0a2f039e0a88f539c40738f647635
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-04-05 21:17:59 -0500

    Ingredients + Steps Styles & Scroll

--- NavigationSample1/Components/MealDetail/Subtitle.jsx
+++ NavigationSample1/Components/MealDetail/Subtitle.jsx
@@ -1,5 +1,27 @@
-function Subtitle() {
+import { Text, StyleSheet, View } from "react-native";
 
+function Subtitle({children}) {
+    return (
+        <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>{children}</Text>
+        </View>
+    );
 }
 
 export default Subtitle;
+
+const styles = StyleSheet.create({
+    subtitle: {
+        color: '#e2b497',
+        fontSize: 18,
+        fontWeight: 'bold', // Make the subtitle bold,
+        textAlign: 'center', // Center align the subtitle
+    },
+    subtitleContainer: {
+        padding: 6,
+        borderBottomColor: '#e2b497', // Add a bottom border to the subtitle for better visibility
+        borderBottomWidth: 2, // Thickness of the bottom border
+        marginVertical: 4, // Use vertical to allow horizontal line to go across entire screen
+        marginHorizontal: 12, // Add some horizontal margin to align past the width the edges of the screen
+    }
+});
--- NavigationSample1/Components/Screens/MealDetailsScreen.jsx
+++ NavigationSample1/Components/Screens/MealDetailsScreen.jsx
@@ -1,13 +1,15 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../../Data/Dummy";
 import MealDetails from "../MealDetails";
+import Subtitle from "../MealDetail/Subtitle";
+import List from "../MealDetail/List";
 
 function MealDetailsScreen({route}) {
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id === mealId); // Find the meal by id from the dummy data
 
     return (
-        <View>
+        <ScrollView style={styles.rootContainer}>
             <Image 
                 source={{uri: selectedMeal.imageUrl}}
                 style={styles.image}/>
@@ -17,23 +19,25 @@ function MealDetailsScreen({route}) {
                 complexity={selectedMeal.complexity} 
                 affordability={selectedMeal.affordability}
                 textStyle={styles.detailTextOverride}/>
-            <View style={styles.subtitleContainer}>
-                <Text style={styles.subtitle}>Ingredients</Text>
-            </View>
-            {selectedMeal.ingredients.map(ingredient => <Text key={ingredient}>{ingredient}</Text>)}
-            
-            <View style={styles.subtitleContainer}>
-                <Text style={styles.subtitle}>Steps</Text>
-            </View>
 
-            {selectedMeal.steps.map(step => <Text key={step}>{step}</Text>)}
-        </View>
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients} />            
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
     );
 }
 
 export default MealDetailsScreen;
 
 const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 30, // Allows us to buffer the scrolling
+    },
     image: {
         width: '100%', // Make the image take the full width of the screen
         height: 350, // Fixed height for the image
@@ -48,17 +52,10 @@ const styles = StyleSheet.create({
     detailTextOverride: {
         color: 'white'
     },
-    subtitle: {
-        color: '#e2b497',
-        fontSize: 18,
-        fontWeight: 'bold', // Make the subtitle bold,
-        textAlign: 'center', // Center align the subtitle
+    listOuterContainer: {
+        alignItems: 'center', // Center align the outer container for the list
     },
-    subtitleContainer: {
-        padding: 6,
-        borderBottomColor: '#e2b497', // Add a bottom border to the subtitle for better visibility
-        borderBottomWidth: 2, // Thickness of the bottom border
-        marginVertical: 4, // Use vertical to allow horizontal line to go across entire screen
-        marginHorizontal: 16, // Add some horizontal margin to align past the width the edges of the screen
-    }
+    listContainer: {
+        width: '80%', // Make the list take 80% of the width of the screen
+    },
 });
