d9b4b304eb86316b99b2f34a511c99fbf6bd40f8
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-04-05 21:54:58 -0500

    Style Drawer (Finished Module)

--- NavigationSample1/App.js
+++ NavigationSample1/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './Components/Screens/CategoriesScreen';
 import MealDetailsScreen from './Components/Screens/MealDetailsScreen';
+
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons for drawer icon
 
 import { NavigationContainer } from '@react-navigation/native';
 import MealsOverviewScreen from './Components/Screens/MealsOverviewScreen';
@@ -20,18 +22,30 @@ function DrawerNavigator() {
       headerTintColor: 'white', // Header text color
       sceneStyle: {
         backgroundColor: '#3f2f25', // Background color for the drawer's scenes
-      }
+      },
+      drawerContentStyle: {
+        backgroundColor: '#3f2f25', // Background color for the drawer itself
+      },
+      drawerInactiveTintColor: 'white', // Color of the inactive drawer items
+      drawerActiveTintColor: '#351401', // Color of the active drawer item
+      drawerActiveBackgroundColor: '#e4baa1', // Background color of the active drawer item
     }}>
       <Drawer.Screen 
         name="Categories"
         component={CategoriesScreen}
         options={{
           title: "Meal Categories", // Title for the drawer item
+          drawerIcon: ({color, size}) =>
+            (<Ionicons name="list" color={color} size={size}  />),
         }}/>
 
       <Drawer.Screen
         name="Favorites"
-        component={FavoritesScreen}/>
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({color, size}) =>
+            (<Ionicons name="star" color={color} size={size}  />),
+        }}/>
     </Drawer.Navigator>;
 }
 
